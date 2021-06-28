23bcf1e46eff0927dc792c3626865aa874e93eb8
CDiala <dialachibuzo@yahoo.com>
2021-06-28 08:35:26 +0100

    Remapped import statements

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import "./component/pages/Scholarship/Scholarship.css";
 import Footer from './component/shared/footer/Footer';
 import { ScholarshipList } from "./component/pages/ScholarshipList/ScholarshipList";
 import Scholarship from './component/pages/Scholarship/Scholarship'
-import { ScholarshipListCard } from "./component/pages/Scholarship/ScholarshipListCard/ScholarshipListCard";
-import { ScholarshipListCardGrid } from "./component/pages/Scholarship/ScholarshipListCardGrid/ScholarshipListCardGrid";
-import { ScholarshipAsideCardGrid } from './component/pages/Scholarship/ScholarshipAsideCardGrid/ScholarshipAsideCardGrid'
+import { ScholarshipListCard } from "./component/pages/ScholarshipList/ScholarshipListCard/ScholarshipListCard";
+import { ScholarshipListCardGrid } from "./component/pages/ScholarshipList/ScholarshipListCardGrid/ScholarshipListCardGrid";
+import { ScholarshipAsideCardGrid } from './component/pages/ScholarshipList/ScholarshipAsideCardGrid/ScholarshipAsideCardGrid'
 // import ProgramCardGrid from './component/ProgramCardGrid/ProgramCardGrid';
 // import LocationCardGrid from './component/LocationCardGrid/LocationCardGrid';
 // import Header from './component/Header/Header'
