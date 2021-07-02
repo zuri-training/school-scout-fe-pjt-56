fe23e6cf0403c89000c4179fdc4cf391e926bf1f
CDiala <dialachibuzo@yahoo.com>
2021-07-02 14:39:50 +0100

    Modified career advisory page 4 component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Scholarship from "./component/pages/Scholarship/Scholarship";
 import CareerAdvisoryHome from "./component/comps/CareerAdvisoryHome";
 import CareerAdvisoryPage2 from "./component/comps/CareerAdvisoryPage2";
 import CareerAdvisoryPage3 from "./component/comps/CareerAdvisoryPage3";
+import CareerAdvisory4 from "./component/comps/CareerAdvisoryPage4";
 import { CareerAdvisoryButton } from "./component/comps/CareerAdvisoryButton";
 import { CareerAdvisoryTop } from './component/comps/CareerAdvisoryTop'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -50,6 +51,12 @@ function App() {
               {/* <CareerAdvisoryTop /> */}
               <CareerAdvisoryPage3 />
               {/* <CareerAdvisoryButton /> */}
+            </Route>
+            <Route exact path={process.env.PUBLIC_URL + '/career-advisory/cap-4'}>
+              <ScholarshipHeaderBlue />
+              <CareerAdvisoryTop />
+              <CareerAdvisory4 />
+              <CareerAdvisoryButton />
             </Route>
             {/* <Route exact path={process.env.PUBLIC_URL + '/'}> */}
             <Route path='/'>
--- src/component/comps/CareerAdvisoryPage4.js
+++ src/component/comps/CareerAdvisoryPage4.js
@@ -1,36 +1,21 @@
-import "./careerAdvisoryPage4.css"
-import leftArrow from "./images/left-arrow.svg"
-function CareerAdvisory4(){
- return (
-  <>
-  <header>
-  <section className="a4-header-container">
-   <div className="a4-main-content">
-    <p>1</p>
-    <a href="./">Tell us some basic information about you</a>
-    <p>2</p>
-    <a href="./">We will find great courses that matches your profile </a>
-    <p>3</p>
-    <a href="./">Pick a course and Explore !</a>
-   </div>
-  </section>
- </header>
- <main>
-  <section className="a4-main-section">
-<h1>In which O'Level Subject pairs did you excel the most?</h1>
-<p>We will use this to streamline our great suggestions for you</p>
-
-<section className="a4-btn-container">
- <button id="btn-1" className="a4-main-btn">Chemistry/Physics</button>
- <button className="a4-main-btn">Account/Economics</button>
- <button className="a4-main-btn">Litrature/Government</button>
-</section>
-  </section>
- </main>
-  <div className="a4-control-btn">
-   <button><img src={leftArrow} alt="" /> Back</button>
-  </div>
-</>
- )
+import "./CareerAdvisoryPage4.css";
+function CareerAdvisory4() {
+  return (
+    <>
+      <main>
+        <section className="a4-main-section">
+          <div className="a4-cap4-container">
+            <h1 className="a4-cap4-heading">In which O'Level Subject pairs did you excel the most?</h1>
+            <p className="a4-cap4-p">We will use this to streamline our great suggestions for you</p>
+            <section className="a4-btn-container">
+              <button id="btn-1" className="a4-main-btn">Chemistry / Physics</button>
+              <button className="a4-main-btn">Account / Economics</button>
+              <button className="a4-main-btn">Litrature / Government</button>
+            </section>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
-export default CareerAdvisory4
+export default CareerAdvisory4;
