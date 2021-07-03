ded6190aae3290ca1daa9a7f727db32fb8a90954
CDiala <dialachibuzo@yahoo.com>
2021-07-03 23:41:16 +0100

    Fixed style issue in CareerAdvisoryPage component

--- src/component/comps/CareerAdvisoryPage2.js
+++ src/component/comps/CareerAdvisoryPage2.js
@@ -1,16 +1,25 @@
 import './CareerAdvisoryPage2.css'
+import { Link } from 'react-router-dom';
+
+function Navigate(event) {
+  alert('hi ' + event);
+  // window.location = process.env.PUBLIC_URL + '/career-advisory/cap-3'
+}
 
 function CareerAdvisory2(){
  return (
    <>
      <main className='a2-main-container'>
        <h1 className="a2-p-cap">Where do you live ?</h1>
-       <select className='a2-select-btn' name='' id='state'>
+       <select onChange={ (e) => Navigate(e.target.value) } className='a2-select-btn' name='' id='state'>
          <option value='abia'>Choose a state</option>
          <option value='Lagos'>Lagos</option>
          <option value='Delta'>Delta</option>
          <option value='Abuja'>Abuja</option>
          <option value='Imo'>Imo</option>
+         <option value="hi">Test
+           <Link to={process.env.PUBLIC_URL + '/career-advisory/cap-3'}></Link>
+         </option>
        </select>
      </main>
    </>
