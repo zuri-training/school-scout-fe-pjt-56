4efdc2e436672233fce7dfca99e39dbb50e39497
CDiala <dialachibuzo@yahoo.com>
2021-07-03 23:44:58 +0100

    Testing CareerAdvisoryPage6

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import CareerAdvisoryPage2 from "./component/comps/CareerAdvisoryPage2";
 import CareerAdvisoryPage3 from "./component/comps/CareerAdvisoryPage3";
 import CareerAdvisory4 from "./component/comps/CareerAdvisoryPage4";
 import CareerAdvisory5 from "./component/comps/CareerAdvisoryPage5";
+import CareerAdvisory6 from "./component/comps/CareerAdvisoryPage6";
 import { CareerAdvisoryButton } from "./component/comps/CareerAdvisoryButton";
 import { CareerAdvisoryTop } from './component/comps/CareerAdvisoryTop'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -63,6 +64,13 @@ function App() {
               <ScholarshipHeaderBlue />
               <CareerAdvisoryTop />
               <CareerAdvisory5 />
+              <CareerAdvisoryButton />
+            </Route>
+            <Route exact path={process.env.PUBLIC_URL + '/career-advisory/cap-6'}>
+              <ScholarshipHeaderBlue />
+              <CareerAdvisoryTop />
+              <CareerAdvisory5 />
+              <CareerAdvisory6 />
               <CareerAdvisoryButton />
             </Route>
             {/* <Route exact path={process.env.PUBLIC_URL + '/'}> */}
