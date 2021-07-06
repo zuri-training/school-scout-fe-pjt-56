39aac3ebec15c7fd9c9193f50d57e45eec764743
CDiala <dialachibuzo@yahoo.com>
2021-07-06 01:38:18 +0100

    Added "go bsck" feature to career advisory button.

--- src/component/comps/CareerAdvisoryButton.js
+++ src/component/comps/CareerAdvisoryButton.js
@@ -1,12 +1,15 @@
-import { Link } from "react-router-dom"
+import { Link, useHistory } from "react-router-dom"
 import './CareerAdvisoryButton.css'
 import arr from "./images/left-arrow.svg";
 
 export const CareerAdvisoryButton = () => {
+
+    let history = useHistory()
+
     return (
         <div className="cad-cab-btn">
             <div className="cad-btn-container">
-                <Link to="" className="cad-btn-back">
+                <Link onClick={ () => history.goBack() } to="" className="cad-btn-back">
                     <img className="" src={ arr } alt="" />
                     <p className="cad-btn-link">Back</p>
                 </Link>
